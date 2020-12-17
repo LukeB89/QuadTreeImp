1186272fc29a8bd384400ce2d6bc4eb51fe49ea9
MBashford <54007477+MBashford@users.noreply.github.com>
2020-12-17 13:50:09 +0000

    update runtime - now runns 10000 iterations & writes results to output csv

--- src/com/git/QuadTreeImp/runtimeTest.java
+++ src/com/git/QuadTreeImp/runtimeTest.java
@@ -1,15 +1,30 @@
 package com.git.QuadTreeImp;
 
 import java.lang.Math;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 
 public class runtimeTest {
 
     public static void main(String args[]){
 
-        for (int i = 1; i <= 20; i++) {
-            System.out.println(i);
-            System.out.println(repeatedTimeFunc(i, 1000));
+        // store number of leaf nodes & runtime for each insert
+        int[][] results = new int[30][2];
+
+        for (int i = 1; i <= 30; i++) {
+            results[i - 1][0] = (int)Math.pow(2, i);
+            results[i - 1][1] = (int)repeatedTimeFunc(i, 10000);
         }
+
+        // output the results to a csv file
+        toCsv(results, "insertTest.csv");
+
+        /*
+        for (int[] arr: results) {
+            System.out.println(arr[0] + ": " + arr[1]);
+        }
+         */
 
     }
 
@@ -20,7 +35,7 @@ public class runtimeTest {
 
         // perform & time a worst-case insert into the tree
         long start = System.nanoTime();
-        tree.insert(new int[]{n - 1 ,n - 1}, true,1);
+        tree.insert(new int[]{0 ,0}, true,1);
         long end = System.nanoTime();
 
         //System.out.println(tree);
@@ -36,6 +51,25 @@ public class runtimeTest {
         }
 
         return total / k;
+    }
+
+    public static void toCsv(int[][] arr, String pathName){
+        try (PrintWriter writer = new PrintWriter(new File(pathName))) {
+            for (int[] row: arr) {
+
+                // build a string to represent each row in the csv
+                String rowString = "";
+                for (int value: row) {
+                    rowString = rowString + value + ",";
+                }
+                // remove the last comma from each row
+                rowString = rowString.substring(0, rowString.length() - 1);
+                rowString += "\n";
+                writer.write(rowString);
+            }
+        } catch (FileNotFoundException err) {
+            System.out.println(err.getMessage());
+        }
     }
 
 }
