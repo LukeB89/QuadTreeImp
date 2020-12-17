26a3a856f951750b6f70885fc7f1856245ea2f2c
MBashford <54007477+MBashford@users.noreply.github.com>
2020-12-17 23:18:42 +0000

    runtimeTest - implement runtime test for basic select operation

--- src/com/git/QuadTreeImp/runtimeTest.java
+++ src/com/git/QuadTreeImp/runtimeTest.java
@@ -7,18 +7,19 @@ import java.io.PrintWriter;
 
 public class runtimeTest {
 
-    public static void main(String args[]){
+    public static void main(String[] args){
 
         // store number of leaf nodes & runtime for each insert
-        int[][] results = new int[30][2];
+        int[][] results = new int[30][3];
 
         for (int i = 1; i <= 30; i++) {
             results[i - 1][0] = (int)Math.pow(2, i);
-            results[i - 1][1] = (int)repeatedTimeInsert(i, 10000);
+            results[i - 1][1] = repeatedTimeInsert(i, 10000);
+            results[i - 1][2] = repeatedTimeSearch(i, 10000);
         }
 
         // output the results to a csv file
-        toCsv(results, "insertTest.csv");
+        toCsv(results, new String[]{"nodes", "insert", "search"},"runTimes.csv");
 
         /*
         for (int[] arr: results) {
@@ -29,6 +30,7 @@ public class runtimeTest {
     }
 
     public static int timeInsert(int n){
+        //performs a worst-case insert for a single node
 
         // instantiate a tree with 2^^n nodes
         QuadTree<Boolean> tree = new QuadTree<>((int)Math.pow(2, n), false);
@@ -44,6 +46,7 @@ public class runtimeTest {
     }
 
     public static int repeatedTimeInsert(int n, int k){
+        //returns the worst-case average runtime for the insert method over k iterations
 
         int total = 0;
         for (int i = 0; i < k; i++) {
@@ -54,9 +57,15 @@ public class runtimeTest {
     }
 
     public static void toCsv(int[][] arr, String pathName){
-        try (PrintWriter writer = new PrintWriter(new File(pathName))) {
-            for (int[] row: arr) {
+        toCsv(arr, new String[]{""} , pathName);
+    }
 
+    public static void toCsv(int[][] arr, String[] header, String pathName){
+        try (PrintWriter writer = new PrintWriter(new File(pathName))) {
+            //write the csv header
+            writer.write(String.join(",", header) + "\n");
+
+            for (int[] row: arr) {
                 // build a string to represent each row in the csv
                 String rowString = "";
                 for (int value: row) {
@@ -70,6 +79,34 @@ public class runtimeTest {
         } catch (FileNotFoundException err) {
             System.out.println(err.getMessage());
         }
+    }
+
+    public static int timeSearch(int n){
+        //performs a worst-case search for a single node
+
+        // instantiate a tree with 2^^n nodes
+        QuadTree<Boolean> tree = new QuadTree<>((int)Math.pow(2, n), false);
+        tree.insert(new int[]{0 ,0}, true,1);
+
+        // perform & time a worst-case search into the tree
+        long start = System.nanoTime();
+        tree.search(new int[]{0, 0});
+        long end = System.nanoTime();
+
+        //System.out.println(tree);
+
+        return (int)(end - start);
+    }
+
+    public static int repeatedTimeSearch(int n, int k){
+        //returns the worst-case average runtime for the search method over k iterations
+
+        int total = 0;
+        for (int i = 0; i < k; i++) {
+            total += timeSearch(n);
+        }
+
+        return total / k;
     }
 
 }
