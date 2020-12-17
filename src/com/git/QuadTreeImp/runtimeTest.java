cfdeae73958e58b67eb980d8b59f895449287618
MBashford <54007477+MBashford@users.noreply.github.com>
2020-12-17 02:07:38 +0000

    update benchmarking to repeat k times for each tree-size

--- src/com/git/QuadTreeImp/runtimeTest.java
+++ src/com/git/QuadTreeImp/runtimeTest.java
@@ -1,8 +1,6 @@
 package com.git.QuadTreeImp;
 
 import java.lang.Math;
-import java.lang.reflect.Method;
-
 
 public class runtimeTest {
 
@@ -10,25 +8,34 @@ public class runtimeTest {
 
         for (int i = 1; i <= 20; i++) {
             System.out.println(i);
-            System.out.println(timeFunc(i));
+            System.out.println(repeatedTimeFunc(i, 1000));
         }
-
 
     }
 
     public static int timeFunc(int n){
 
         // instantiate a tree with 2^^n nodes
-        QuadTree<Boolean> tree = new QuadTree<Boolean>((int)Math.pow(2, n), false);
+        QuadTree<Boolean> tree = new QuadTree<>((int)Math.pow(2, n), false);
 
         // perform & time a worst-case insert into the tree
         long start = System.nanoTime();
         tree.insert(new int[]{n - 1 ,n - 1}, true,1);
         long end = System.nanoTime();
 
-        System.out.println(tree);
+        //System.out.println(tree);
 
         return (int)(end - start);
+    }
+
+    public static int repeatedTimeFunc(int n, int k){
+
+        int total = 0;
+        for (int i = 0; i < k; i++) {
+            total += timeFunc(n);
+        }
+
+        return total / k;
     }
 
 }
